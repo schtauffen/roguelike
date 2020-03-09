ddc4beccca232563b06ae66be4549264d27be5b6
Zach Dahl <z.schtauffen@gmail.com>
2020-03-09 14:40:44 -0500

    generate dungeons

---
+++ src/main.rs
@@ -0,0 +1,269 @@
+use rand::Rng;
+use std::cmp;
+use tcod::colors::*;
+use tcod::console::*;
+use tcod::input::{Key, KeyCode::*};
+
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+
+const MAP_WIDTH: i32 = 80;
+const MAP_HEIGHT: i32 = 45;
+
+const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
+const COLOR_DARK_GROUND: Color = Color {
+    r: 50,
+    g: 50,
+    b: 150,
+};
+
+const FPS_MAX: i32 = 20;
+
+const ROOM_MAX_SIZE: i32 = 10;
+const ROOM_MIN_SiZE: i32 = 6;
+const MAX_ROOMS: i32 = 30;
+
+struct Tcod {
+    root: Root,
+    con: Offscreen,
+}
+
+fn handle_keys(tcod: &mut Tcod, game: &Game, player: &mut Object) -> bool {
+    let key = tcod.root.wait_for_keypress(true);
+    match key {
+        // fullscreen
+        Key {
+            code: Enter,
+            alt: true,
+            ..
+        } => {
+            let fullscreen = tcod.root.is_fullscreen();
+            tcod.root.set_fullscreen(!fullscreen);
+        }
+
+        // escape
+        Key { code: Escape, .. } => return true,
+
+        // movement
+        Key { code: Up, .. } => player.move_by(0, -1, game),
+        Key { code: Down, .. } => player.move_by(0, 1, game),
+        Key { code: Left, .. } => player.move_by(-1, 0, game),
+        Key { code: Right, .. } => player.move_by(1, 0, game),
+
+        _ => {}
+    }
+
+    false
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Tile {
+    blocked: bool,
+    block_sight: bool,
+}
+
+impl Tile {
+    pub fn empty() -> Self {
+        Tile {
+            blocked: false,
+            block_sight: false,
+        }
+    }
+
+    pub fn wall() -> Self {
+        Tile {
+            blocked: true,
+            block_sight: true,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Object {
+    x: i32,
+    y: i32,
+    char: char,
+    color: Color,
+}
+
+impl Object {
+    pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
+        Object { x, y, char, color }
+    }
+
+    pub fn move_by(&mut self, dx: i32, dy: i32, game: &Game) {
+        if !game.map[(self.x + dx) as usize][(self.y + dy) as usize].blocked {
+            self.x += dx;
+            self.y += dy;
+        }
+    }
+
+    pub fn draw(&self, con: &mut dyn Console) {
+        con.set_default_foreground(self.color);
+        con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Rect {
+    x1: i32,
+    y1: i32,
+    x2: i32,
+    y2: i32,
+}
+
+impl Rect {
+    pub fn new(x1: i32, y1: i32, w: i32, h: i32) -> Self {
+        Rect {
+            x1,
+            y1,
+            x2: x1 + w,
+            y2: y1 + h,
+        }
+    }
+
+    pub fn center(&self) -> (i32, i32) {
+        let center_x = (self.x1 + self.x2) / 2;
+        let center_y = (self.y1 + self.y2) / 2;
+        (center_x, center_y)
+    }
+
+    pub fn intersects_with(&self, other: &Rect) -> bool {
+        self.x1 <= other.x2 && self.x2 >= other.x1 && self.y1 <= other.y2 && self.y2 >= other.y1
+    }
+}
+
+fn create_room(room: Rect, map: &mut Map) {
+    for x in (room.x1 + 1)..room.x2 {
+        for y in (room.y1 + 1)..room.y2 {
+            map[x as usize][y as usize] = Tile::empty();
+        }
+    }
+}
+
+fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+    for x in cmp::min(x1, x2)..=cmp::max(x1, x2) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
+    for y in cmp::min(y1, y2)..=cmp::max(y1, y2) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+// TODO - everything as an object & remove map?
+// TODO - one continues array with map[y * MAP_WIDTH + x]?
+// TODO - quadrant collision detection ?
+type Map = Vec<Vec<Tile>>;
+
+struct Game {
+    map: Map,
+}
+
+fn make_map(player: &mut Object) -> Map {
+    let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+
+    let mut rooms = vec![];
+
+    for _ in 0..MAX_ROOMS {
+        let w = rand::thread_rng().gen_range(ROOM_MIN_SiZE, ROOM_MAX_SIZE + 1);
+        let h = rand::thread_rng().gen_range(ROOM_MIN_SiZE, ROOM_MAX_SIZE + 1);
+        let x = rand::thread_rng().gen_range(0, MAP_WIDTH - w);
+        let y = rand::thread_rng().gen_range(0, MAP_HEIGHT - h);
+
+        let new_room = Rect::new(x, y, w, h);
+
+        let failed = rooms
+            .iter()
+            .any(|other_room| new_room.intersects_with(other_room));
+        if !failed {
+            create_room(new_room, &mut map);
+
+            let (new_x, new_y) = new_room.center();
+
+            if rooms.is_empty() {
+                player.x = new_x;
+                player.y = new_y;
+            } else {
+                let (prev_x, prev_y) = rooms[rooms.len() - 1].center();
+
+                if rand::random() {
+                    create_h_tunnel(prev_x, new_x, prev_y, &mut map);
+                    create_v_tunnel(prev_y, new_y, new_x, &mut map);
+                } else {
+                    create_v_tunnel(prev_y, new_y, prev_x, &mut map);
+                    create_h_tunnel(prev_x, new_x, new_y, &mut map);
+                }
+            }
+
+            rooms.push(new_room);
+        }
+    }
+
+    map
+}
+
+fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object]) {
+    for object in objects {
+        object.draw(&mut tcod.con);
+    }
+
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            let wall = game.map[x as usize][y as usize].block_sight;
+            if wall {
+                tcod.con
+                    .set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
+            } else {
+                tcod.con
+                    .set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
+            }
+        }
+    }
+
+    blit(
+        &tcod.con,
+        (0, 0),
+        (MAP_WIDTH, MAP_HEIGHT),
+        &mut tcod.root,
+        (0, 0),
+        1.0,
+        1.0,
+    );
+}
+
+fn main() {
+    tcod::system::set_fps(FPS_MAX);
+
+    let root = Root::initializer()
+        .font("arial10x10.png", FontLayout::Tcod)
+        .font_type(FontType::Greyscale)
+        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .title("Rust/libtcod tutorial")
+        .init();
+
+    let con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
+
+    let mut tcod = Tcod { root, con };
+
+    let player = Object::new(25, 23, '@', WHITE);
+    let mut objects = [player];
+    let game = Game {
+        map: make_map(&mut objects[0]),
+    };
+    // let npc = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', YELLOW);
+
+    while !tcod.root.window_closed() {
+        tcod.con.clear();
+        render_all(&mut tcod, &game, &objects);
+        tcod.root.flush();
+
+        let player = &mut objects[0];
+        let exit = handle_keys(&mut tcod, &game, player);
+        if exit {
+            break;
+        }
+    }
+}
---
+++ tutorial/part-2a-generalising.rs
@@ -0,0 +1,123 @@
+// This file is generated automatically. Do not edit it directly.
+// See the Contributing section in README on how to make changes to it.
+use tcod::colors::*;
+use tcod::console::*;
+
+// actual size of the window
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+
+const LIMIT_FPS: i32 = 20; // 20 frames-per-second maximum
+
+struct Tcod {
+    root: Root,
+    con: Offscreen,
+}
+
+/// This is a generic object: the player, a monster, an item, the stairs...
+/// It's always represented by a character on screen.
+struct Object {
+    x: i32,
+    y: i32,
+    char: char,
+    color: Color,
+}
+
+impl Object {
+    pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
+        Object { x, y, char, color }
+    }
+
+    /// move by the given amount
+    pub fn move_by(&mut self, dx: i32, dy: i32) {
+        self.x += dx;
+        self.y += dy;
+    }
+
+    /// set the color and then draw the character that represents this object at its position
+    pub fn draw(&self, con: &mut dyn Console) {
+        con.set_default_foreground(self.color);
+        con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
+    }
+}
+
+fn handle_keys(tcod: &mut Tcod, player: &mut Object) -> bool {
+    use tcod::input::Key;
+    use tcod::input::KeyCode::*;
+
+    let key = tcod.root.wait_for_keypress(true);
+    match key {
+        Key {
+            code: Enter,
+            alt: true,
+            ..
+        } => {
+            // Alt+Enter: toggle fullscreen
+            let fullscreen = tcod.root.is_fullscreen();
+            tcod.root.set_fullscreen(!fullscreen);
+        }
+        Key { code: Escape, .. } => return true, // exit game
+
+        // movement keys
+        Key { code: Up, .. } => player.move_by(0, -1),
+        Key { code: Down, .. } => player.move_by(0, 1),
+        Key { code: Left, .. } => player.move_by(-1, 0),
+        Key { code: Right, .. } => player.move_by(1, 0),
+
+        _ => {}
+    }
+
+    false
+}
+
+fn main() {
+    tcod::system::set_fps(LIMIT_FPS);
+
+    let root = Root::initializer()
+        .font("arial10x10.png", FontLayout::Tcod)
+        .font_type(FontType::Greyscale)
+        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .title("Rust/libtcod tutorial")
+        .init();
+
+    let con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    let mut tcod = Tcod { root, con };
+
+    // create object representing the player
+    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', WHITE);
+
+    // create an NPC
+    let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', YELLOW);
+
+    // the list of objects with those two
+    let mut objects = [player, npc];
+
+    while !tcod.root.window_closed() {
+        // clear the screen of the previous frame
+        tcod.con.clear();
+
+        for object in &objects {
+            object.draw(&mut tcod.con);
+        }
+
+        // blit the contents of "con" to the root console and present it
+        blit(
+            &tcod.con,
+            (0, 0),
+            (SCREEN_WIDTH, SCREEN_HEIGHT),
+            &mut tcod.root,
+            (0, 0),
+            1.0,
+            1.0,
+        );
+        tcod.root.flush();
+
+        // handle keys and exit game if needed
+        let player = &mut objects[0];
+        let exit = handle_keys(&mut tcod, player);
+        if exit {
+            break;
+        }
+    }
+}
